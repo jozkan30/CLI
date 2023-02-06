45084077100e8adfe8ef08c9dabc21a9495658b4
jozkan30 <jozkan2@gmail.com>
2023-02-06 12:09:00 -0500

    get reqs working

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ db = PostgresqlDatabase('bookmarks', user='justinozkan',
                         password='justin30', host='localhost', port=5432)
 db.connect()
 
+
 class BaseModel(Model):
     class Meta:
         database = db
 
+
 class Mark(BaseModel):
-    url =  TextField()
+    url = TextField()
     description = TextField()
+
 
 db.drop_tables([Mark])
 db.create_tables([Mark])
-
-
 
 
 nyt = Mark(url='https://www.nytimes.com/', description='New York Times')
@@ -29,46 +30,39 @@ github.save()
 amazon = Mark(url='https://www.amazon.com/', description='Amazon ')
 amazon.save()
 
+# Add a Bookmark(CREATE)
 def add_bookmark():
-        add_url = input( "Please enter a valid URl")
-        add_description = input("Please enter a description")
-        add_new_mark = Mark(url=add_url, description =add_description)
-        add_new_mark.save()
-        print("Bookmark Added!")
+    add_url = input("Please enter a valid URl")
+    add_description = input("Please enter a description")
+    add_new_mark = Mark(url=add_url, description=add_description)
+    add_new_mark.save()
+    print("Bookmark Added!")
+
 
 def exit():
-    print ("Thanks for stopping by!")
+    print("Thanks for stopping by!")
 
 
-def intro(): 
+def intro():
     print("Pleae try again!")
 
-    
-    
+def delete(id):
+    req_id = input("What is ID of the bookmark you would like to delete? ")
+    find = Mark.get(Mark.id == id)
+    remove = Mark.delete_instance(find) 
+    print(f' Bookmark {id} eliminated!')
 
-print("Would you like to add a URl Please type 'yes' , 'no' or 'list, to view all entries' ")
 
-
+print("Would you like to add a URl Please type 'yes' , 'no' or 'list, to view all entries' or 'del ")
 
 make = str(input()).lower()
 if make == 'yes':
     add_bookmark()
 elif make == 'no':
     exit()
+ # View All Entries (READ)   
 elif make == 'list':
     mark_list = []
     for mark in Mark.select():
         mark_list.append(model_to_dict(mark))
-        print (mark_list)
-
-
-    
-
-
-
-
-
-
-
-
-
+        print(mark_list)
